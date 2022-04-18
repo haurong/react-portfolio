d48d74ec21d1647a27c2ba72ef6aa77544fe76fb
chloe012125 <chloe012125@gmail.com>
2022-04-18 18:05:14 +0800

    切版

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,9 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome , faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome , faUser  } from '@fortawesome/free-solid-svg-icons';
+import { faBehance } from '@fortawesome/free-brands-svg-icons';
+
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -15,13 +17,20 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.behance.net/chloe012125'>
+                    <FontAwesomeIcon icon={faBehance} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
