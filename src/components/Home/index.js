0bbf28adc9a2e9b2567834999b7d54c3ee5c10b9
張華容 <chloe012125@gmail.com>
2022-04-20 21:52:38 +0800

    “訊息”

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
 
 const Home = () => {
+    const [ letterClass , setLetterClass ] = useState('text-animate');
+    const nameArray = ['l', 'o','b','o','d','a','n'];
+    const jobArray = ['d','e','s','i','g','n','e','r'];
 
     return (
         <div className="container home-page">
